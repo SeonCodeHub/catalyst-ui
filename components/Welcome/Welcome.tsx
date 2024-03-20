b9009fcf221b2f462c4aaaf855639e8de7f3d755
Talgar <talgarmarlis@gmail.com>
2024-03-20 22:10:23 +0100

    Initial setup for web deployment

--- components/Welcome/Welcome.tsx
+++ components/Welcome/Welcome.tsx
@@ -1,4 +1,4 @@
-import { Title, Text, Anchor } from '@mantine/core';
+import { Title, Text } from '@mantine/core';
 import classes from './Welcome.module.css';
 
 export function Welcome() {
@@ -7,16 +7,12 @@ export function Welcome() {
       <Title className={classes.title} ta="center" mt={100}>
         Welcome to{' '}
         <Text inherit variant="gradient" component="span" gradient={{ from: 'pink', to: 'yellow' }}>
-          Mantine
+          Catalyst
         </Text>
       </Title>
       <Text c="dimmed" ta="center" size="lg" maw={580} mx="auto" mt="xl">
-        This starter Next.js project includes a minimal setup for server side rendering, if you want
-        to learn more on Mantine + Next.js integration follow{' '}
-        <Anchor href="https://mantine.dev/guides/next/" size="lg">
-          this guide
-        </Anchor>
-        . To get started edit page.tsx file.
+        A powerful project management tool designed to elevate your workflow. Organize tasks,
+        streamline processes, and optimize collaboration for seamless project execution.
       </Text>
     </>
   );
